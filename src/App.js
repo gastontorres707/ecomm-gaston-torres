e0e01a3e2a276898ded6cfd50fe74b072dbbd343
Gaston <gastontorres707@gmail.com>
2022-09-07 23:09:46 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <nav class="navbar navbar-expand-lg bg-info">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Gasti Shop</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Categoria 1</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Categoria 2</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Categoria 3</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Categoria 4</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
   );
 }
 
